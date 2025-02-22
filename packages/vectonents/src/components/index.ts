12b42c5fe931a3c18bb16eac96ab9bbd0ceeca19
pooya-poi <pooya585@gmai.com>
2025-02-22 13:42:14 +0330

    add: new arrow components (ArrowDown, ArrowLeft, ArrowLeftDown) and update index export

--- packages/vectonents/src/components/index.ts
+++ packages/vectonents/src/components/index.ts
@@ -1,1 +1,4 @@
 export {default as ArrowRight} from "./ArrowRight";
+export {default as ArrowDown} from "./ArrowDown";
+export {default as ArrowLeft} from "./ArrowLeft";
+export {default as ArrowLeftDown} from "./ArrowLeftDown";
