59f164067538fed7af91699700821f052b4671da
pooya-poi <pooya585@gmai.com>
2025-02-22 14:39:03 +0330

    add: import new arrow components and update usage in Home page

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,5 +1,5 @@
 
-import { ArrowLeftDown } from "@pooya-poi/vectonents";
+import { ArrowLeftDown, ArrowRightLeft, ArrowRightUp, ArrowUp } from "@pooya-poi/vectonents";
 import { ArrowDown, ArrowLeft, ArrowRight } from "@pooya-poi/vectonents";
 // import styles from "./page.module.css";
 
@@ -9,16 +9,34 @@ export default function Home() {
   return (
     // <div className={styles.page}>
     <div >
-      <ArrowDown variants="filled"/>
-      <ArrowDown variants="filled-1"/>
-      <ArrowDown variants="outlined"/>
-      <ArrowDown variants="outlined-1"/>
+      <ArrowDown variants="filled" />
+      <ArrowDown variants="filled-1" />
+      <ArrowDown variants="outlined" />
+      <ArrowDown variants="outlined-1" />
       <br />
 
-      <ArrowLeft variants="filled"/>
-      <ArrowLeft variants="outlined"/>
+      <ArrowLeft variants="filled" />
+      <ArrowLeft variants="outlined" />
 
-      <ArrowLeftDown variants="outlined"/>
+      <ArrowLeftDown variants="filled" />
+      <ArrowLeftDown variants="outlined" />
+      <br />
+
+      <ArrowRight variants="filled" />
+      <ArrowRight variants="outlined" />
+      <br />
+
+      <ArrowRightUp variants="filled" />
+      <ArrowRightUp variants="outlined" />
+      <br />
+      <hr />
+      <ArrowRightLeft variants="filled" />
+      <ArrowRightLeft variants="outlined" />
+      <ArrowRightLeft />
+      <hr />
+      <ArrowUp />
+      <ArrowUp variants="filled" />
+      <ArrowUp variants="outlined" />
     </div>
   );
 }
