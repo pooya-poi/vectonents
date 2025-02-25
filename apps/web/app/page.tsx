c8aaf05bdc1d2bdaddd429d6937df6f2bf4ee5bb
pooya-poi <pooya585@gmail.com>
2025-02-25 03:54:11 +0330

    add: implement Tailwind CSS configuration and utility functions, along with a ThemeProvider component for theming support

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,52 +1,42 @@
+"use client"
+import { Button } from "@/components/ui/button";
 import { ArrowDown, ArrowLeft, ArrowRight } from "@pooya-poi/vectonents";
 // import styles from "./page.module.css";
 import * as icons from "@pooya-poi/vectonents";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@radix-ui/react-dropdown-menu";
+import { Moon, Sun } from "lucide-react";
+import { useTheme } from "next-themes";
 
 export default function Home() {
 
-  const iconEntries = Object.entries(icons);
 
-  const iconVariants: { [key: string]: string[] } = {
-    ArrowDown: ['filled', 'outlined', 'filled-1', 'outlined-1'],
-    Sort: ['filled', 'outlined', 'filled-1', 'outlined-1'],
-    AirpodCase: ['filled', 'outlined', 'filled-1', 'outlined-1'],
-    Speaker: ['filled', 'outlined', 'filled-1', 'outlined-1'],
-    Watch: ['filled', 'outlined', 'filled-r', 'outlined-r'],
-    Document: ['filled', 'filled-1', 'filled-2', 'filled-3', 'outlined', 'outlined-1', 'outlined-2', 'outlined-3'],
-    Brush: ['filled', 'filled-1', 'outlined', 'outlined-1'],
-    Crop: ['filled', 'filled-1', 'outlined', 'outlined-1'],
-    Chat: ['filled', 'filled-1', 'filled-2', 'outlined', 'outlined-1', 'outlined-2'],
-    Filter: ['filled', 'filled-1', 'filled-2', 'outlined', 'outlined-1', 'outlined-2'],
-    Menu: ['filled', 'filled-1', 'filled-2', 'outlined', 'outlined-1', 'outlined-2'],
-    Grid: ['filled', 'filled-1', 'filled-2', 'filled-3', 'filled-4', 'filled-5', 'filled-6', 'filled-7', 'outlined', 'outlined-1', 'outlined-2', 'outlined-3', 'outlined-4', 'outlined-5', 'outlined-6', 'outlined-7'],
-
-    // Add more icons and their variants as needed
-  };
-
+  const { setTheme } = useTheme()
 
   return (
-    <div style={{ display: 'flex', flexWrap: 'wrap', gap: '16px' }}>
-      {iconEntries.map(([name, IconComponent]) => {
-        // Get the variants for the current icon from the manual mapping
-        const variants = iconVariants[name] || ['filled', 'outlined']; // Fallback to default variants
+    // <div style={{ display: 'flex', flexWrap: 'wrap', gap: '16px' }}>
+    <div className="bg-red-400 p-5 " >
+      {/* hello */}
 
-        return (
-          <div key={name} style={{ textAlign: 'center', border: '1px solid #ccc', padding: '16px', borderRadius: '8px' }}>
-            <h3 style={{ marginBottom: '16px', fontSize: '16px', color: '#333' }}>{name}</h3>
-            <div style={{ display: 'flex', gap: '16px', justifyContent: 'center' }}>
-              {/* Render each variant */}
-              {variants.map((variant) => (
-                <div key={variant}>
-                  <div style={{ marginBottom: '8px' }}>
-                    <IconComponent size={32} variants={variant as 'filled' | 'outlined'} />
-                  </div>
-                  <div style={{ fontSize: '12px', color: '#666' }}>{variant}</div>
-                </div>
-              ))}
-            </div>
-          </div>
-        );
-      })}
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="outline" size="icon">
+            <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+            <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+            <span className="sr-only">Toggle theme</span>
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuItem onClick={() => setTheme("light")}>
+            Light
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={() => setTheme("dark")}>
+            Dark
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={() => setTheme("system")}>
+            System
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </div>
   );
 }
