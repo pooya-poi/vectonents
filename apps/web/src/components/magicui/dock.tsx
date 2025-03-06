38f9bb846f7d37c8ff3d51a44c1987e46a77a31b
pooya-poi <pooya585@gmail.com>
2025-03-07 02:00:21 +0330

    style: adjust gap in Dock component for improved layout consistency

--- apps/web/src/components/magicui/dock.tsx
+++ apps/web/src/components/magicui/dock.tsx
@@ -27,7 +27,7 @@ const DEFAULT_MAGNIFICATION = 60;
 const DEFAULT_DISTANCE = 140;
 
 const dockVariants = cva(
-  "supports-backdrop-blur:bg-white/10 supports-backdrop-blur:dark:bg-black/10 mx-auto mt-8 flex h-[58px] w-max items-center justify-center gap-2 rounded-2xl border p-2 backdrop-blur-md",
+  "supports-backdrop-blur:bg-white/10 supports-backdrop-blur:dark:bg-black/10 mx-auto mt-8 flex h-[58px] w-max items-center justify-center gap-4 rounded-2xl border p-2 backdrop-blur-md",
 );
 
 const Dock = React.forwardRef<HTMLDivElement, DockProps>(
@@ -47,9 +47,12 @@ const Dock = React.forwardRef<HTMLDivElement, DockProps>(
 
     const renderChildren = () => {
       return React.Children.map(children, (child) => {
-        if (React.isValidElement<DockIconProps>(child) && child.type === DockIcon) {
+        if (
+          React.isValidElement<DockIconProps>(child) &&
+          child.type === DockIcon
+        ) {
           return React.cloneElement(child, {
-            ...(typeof child.props === 'object' ? child.props : {}),
+            ...(typeof child.props === "object" ? child.props : {}),
             mouseX: mouseX,
             size: iconSize,
             magnification: iconMagnification,
