f4f77ebb29fe96e9a8a904c95686668a502c1320
pooya-poi <pooya585@gmail.com>
2025-03-26 23:12:36 +0330

    feat: add ToolsPage component with ColorConverter and LoremIpsumGenerator

---
+++ apps/web/app/tools/page.tsx
@@ -0,0 +1,11 @@
+import ColorConverter from "@/components/widgets/color-convertor";
+import { LoremIpsumGenerator } from "@/components/widgets/lorem-generator";
+
+export default function ToolsPage() {
+  return (
+    <div>
+      <ColorConverter />
+      <LoremIpsumGenerator />
+    </div>
+  );
+}
