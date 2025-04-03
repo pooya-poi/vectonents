79e112f7c9ced60699685ac2da4debc3baf9d477
pooya-poi <pooya585@gmail.com>
2025-04-04 02:16:13 +0330

    refactor: remove unused Copy icon import

--- apps/web/src/components/widgets/lorem-generator.tsx
+++ apps/web/src/components/widgets/lorem-generator.tsx
@@ -3,7 +3,7 @@
 import { useState } from "react";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
-import { Copy, Activity } from "@pooya-poi/vectonents";
+import { Activity } from "@pooya-poi/vectonents";
 import {
   Select,
   SelectContent,
