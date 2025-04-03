5ccb55aca73421fad0368dd3627d4138b3b28733
pooya-poi <pooya585@gmail.com>
2025-04-04 03:05:46 +0330

    refactor: rename PackagePage to GuidePage and update content structure

--- apps/web/app/guide/page.tsx
+++ apps/web/app/guide/page.tsx
@@ -1,3 +1,12 @@
-export default function PackagePage() {
-    return 'Guid Page';
+export default function GuidePage() {
+    return (
+        <div className="mb-56 w-3xl  container mx-auto max-w-[1920px] space-y-8 p-2">
+          <h1 className="text-3xl font-bold">What is Vectonent?</h1>
+          <p className="mt-4">
+            Vectonents is an open-source icon library that provides vector files for
+            displaying icons in your projects. This library aims to make it easier
+            for you to use icons into your projects by the provided package.
+          </p>
+        </div>
+      );
 }
